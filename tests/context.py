7ed0f8b5df77a728e3a76df3e024a7fc87a984c6
bstorm <bstorm@wikimedia.org>
2020-02-24 18:56:32 -0700

    refactor: split up the huge file
    Split this out into lots of modules so that we when I add admin users
    it is actually readable. The first attempt (I2a5b89472cc5cc6e1614ea3c4c4973f7c857fe45
    ) had a mistake in the
    Dockerfile. This version works.
    
    Bug: T214513
    Change-Id: I2203ad71a5e1ee029a564a7b4dee51d7e6e7b513

--- tests/context.py
+++ tests/context.py
@@ -1,8 +1,14 @@
-import os
-import sys
+import os  # noqa: F401
+import sys  # noqa: F401
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 )
 
-from maintain_kubeusers import maintain_kubeusers  # noqa: E402,F401
+from maintain_kubeusers.user import User  # noqa: E402,F401
+from maintain_kubeusers.cli import k_config  # noqa: E402,F401
+from maintain_kubeusers.k8s_api import K8sAPI, client  # noqa: E402,F401
+from maintain_kubeusers.utils import (  # noqa: E402,F401
+    generate_pk,
+    get_tools_from_ldap,
+)
